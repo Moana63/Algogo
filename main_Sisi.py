1ba68334fdce211679de546c076ddf331de70508
Siegfried Dubois <dubois.siegfried@gmail.com>
2022-11-10 16:21:02 +0100

    Update framework de test + gitignore + distance algébrique

--- main_Sisi.py
+++ main_Sisi.py
@@ -2,10 +2,34 @@ from argparse import ArgumentParser
 from os import getpid
 from psutil import Process
 from random import choice
+from collections import Counter
+
+
 
 
 def hash_kmer(kmer: str) -> int:
     return sum(({'A': 1, 'C': 2, 'G': 3, 'T': 4, }.get(l, 0)*(5**i) for i, l in enumerate(kmer)))
+
+def frequency(read:str,seed_size) -> Counter:
+    return Counter([read[k:k+seed_size] for k in range(len(read)-seed_size)])
+
+def calculate_algebric_distance(a:Counter,b:Counter) -> Counter:
+    return sum([abs(a[elt]-b[elt]) for elt in set().union(*a,*b)])
+
+def algebric_clustering(input: str, output: str, ksize:int=4):
+    init_memory = get_memory()
+    reads:list = clean_fasta(input)
+    ref:Counter = frequency(reads[0],ksize)
+    reads_ordered:list = sorted(reads, key=lambda read: calculate_algebric_distance(frequency(read,ksize), ref))
+    max_memory = get_memory()
+    with open(output, 'w') as writer:
+        writer.write(''.join(reads_ordered))
+    return abs(max_memory - init_memory)
+    #vectors:list = [frequency(read,ksize) for read in reads]
+    #while(sum(is_collected) != len(is_collected)):
+    #distance_matrix:list = [[calculate_algebric_distance(vectors[i],vectors[j]) for i in range(len(vectors))] for j in range(len(vectors))]
+    #is_collected:list = [False for _ in vectors]
+
 
 
 def get_memory() -> float:
--- tests_private.py
+++ tests_private.py
@@ -1,6 +1,6 @@
 from time import monotonic
 from datetime import timedelta
-from main import write_ordered_kmers, compress_naive
+from main_Sisi import compress_naive, algebric_clustering
 from typing import Callable
 from os import listdir, system, path
 import matplotlib.pyplot as plt
@@ -10,8 +10,7 @@ FILES: list[str] = sorted([f for f in listdir(
 FILES_NO_EXT: list[str] = [f.split('.')[0] for f in FILES]
 INPUT: list[str] = [f"Ecoli_100Kb/{p}" for p in FILES]
 OUTPUT: list[str] = [f"{inp.split('.')[0]}.txt" for inp in INPUT]
-FUNC_TEST: list[Callable] = [compress_naive, write_ordered_kmers]
-
+FUNC_TEST: list[Callable] = [(compress_naive,{}), (algebric_clustering,{'ksize':3})]
 
 def timer(func):
     """
@@ -27,19 +26,19 @@ def timer(func):
 
 
 @timer
-def my_tester(func: Callable, input: str, output: str) -> None:
-    return {'memory': func(input, output), 'file': output}
+def my_tester(func: Callable, input: str, output: str, kwargs:dict) -> None:
+    return {'memory': func(input, output,**kwargs), 'file': output}
 
 
 if __name__ == "__main__":
-    fig, (ax1, ax2, ax3) = plt.subplots(ncols=3, nrows=1)
+    fig, (ax1, ax2, ax3) = plt.subplots(ncols=3, nrows=1, figsize=(15,10))
     ax1.title.set_text('Fig. A : Sort function time spent')
     ax2.title.set_text('Fig. B : Sort function memory usage')
     ax3.title.set_text('Fig. C : Compression efficiency')
 
-    for func in FUNC_TEST:
+    for (func,kwargs) in FUNC_TEST:
         func_ret: list[tuple] = [
-            my_tester(func, INPUT[i], OUTPUT[i]) for i, _ in enumerate(INPUT)]
+            my_tester(func, INPUT[i], OUTPUT[i],kwargs) for i, _ in enumerate(INPUT)]
         [system(f"gzip -f {out}") for out in OUTPUT]
         x: list = [ret['file'].split('.')[0].split('_')[-1]
                    for ret in func_ret]
@@ -48,13 +47,14 @@ if __name__ == "__main__":
         serie_memory: list = [ret['memory'] for ret in func_ret]
         serie_du: list = [(path.getsize(
             f"{f}.txt.gz")/path.getsize(f"{f}.fasta.gz"))*100 for f in clean_files]
-        ax1.plot(x, serie_time, label=func.__name__)
-        ax2.plot(x, serie_memory, label=func.__name__)
-        ax3.plot(x, serie_du, label=func.__name__)
+        ax1.plot(x, serie_time, label=f"{func.__name__}, {kwargs}")
+        ax2.plot(x, serie_memory, label=f"{func.__name__}, {kwargs}")
+        ax3.plot(x, serie_du, label=f"{func.__name__}, {kwargs}")
 
     handles, labels = ax1.get_legend_handles_labels()
     ax1.set_ylabel("Time (in microseconds)")
     ax2.set_ylabel("Peak memory used (Mb)")
     ax3.set_ylabel("Percentage of original file size")
     fig.legend(handles, labels, loc='upper center')
+    plt.savefig("test.png")
     plt.show()
