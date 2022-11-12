d3b3de3541e48b1aabb8bb040f8205f2db4c2eef
Moana63 <83143968+Moana63@users.noreply.github.com>
2022-11-12 11:28:26 +0100

    First results

---
+++ LIST_RESULTS_TESTS_TRI/1-minimiser_et_GC/main_bis.py
@@ -0,0 +1,57 @@
+from argparse import ArgumentParser
+from os import getpid
+from psutil import Process
+from random import choice
+from test_algo_minimap_2 import clustering_phase1, GC
+
+
+def hash_kmer(kmer: str) -> int:
+    return sum(({'A': 1, 'C': 2, 'G': 3, 'T': 4, }.get(l, 0)*(5**i) for i, l in enumerate(kmer)))
+
+
+def get_memory() -> float:
+    return Process(getpid()).memory_info().rss / 1024 ** 2
+
+
+def clean_fasta(input: str) -> list[str]:
+    with open(input, 'r') as reader:
+        return [l for l in reader if l[0] not in ['\n', '>']]
+
+
+def compress_naive(input: str, output: str):
+    init_memory = get_memory()
+    lines = sorted(clean_fasta(input))
+    max_memory = get_memory()
+    with open(output, 'w') as writer:
+        writer.write(''.join(lines))
+    return abs(max_memory - init_memory)
+
+
+def write_file(filein, fileout):
+    init_memory = get_memory()
+    with open(filein) as f:
+        list_seq = [seq.strip() for seq in f]
+    clusters = clustering_phase1(list_seq)
+    max_memory = get_memory()
+    with open(fileout, "w") as out_file:
+        for group in clusters:
+            group = sorted(group, key= lambda x: GC(list_seq[x]))
+            for seq in group:
+                out_file.write(list_seq[seq]+'\n')
+    return abs(max_memory - init_memory)
+
+
+if __name__ == "__main__":
+    """
+    parser = ArgumentParser()
+    parser.add_argument("-i", "--input",
+                        help="Path to a fasta-like file to re-order", type=str)
+    parser.add_argument("-o", "--output",
+                        help="Path to a txt file, output of the program", type=str)
+    args = parser.parse_args()
+    """
+    kmers = [''.join(choice(['A', 'T', 'C', 'G'])
+                     for _ in range(100)) for _ in range(1000000)]
+    hashes = {k: hash_kmer(k) for k in kmers}
+    print(len(hashes.keys()) == len(set(l for l in hashes.values())))
+    write_file()
---
+++ LIST_RESULTS_TESTS_TRI/3-prop_GC/test_tri_GC.py
@@ -0,0 +1,2 @@
+def GC(ref_seq):
+    return (ref_seq.count('G') + ref_seq.count('C')) / len(ref_seq)
