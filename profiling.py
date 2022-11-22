ae96ab1e38f24927654212f28ed7acb6e480a081
tharos <tharos@tcorp.com>
2022-11-22 08:44:08 +0100

    timings

--- profiling.py
+++ profiling.py
@@ -7,13 +7,15 @@ a = Counter({'CATC': 3, 'CCGG': 3, 'CGGC': 3, 'ATCG': 2, 'TCGA': 2, 'ATCA': 2, '
 b = Counter({'GATG': 3, 'ACGA': 3, 'ATGC': 2, 'TGCA': 2, 'GCAT': 2, 'AACG': 2, 'TGAT': 2, 'GATT': 2, 'ATTT': 2, 'GTAC': 2, 'CGAA': 2, 'GAAC': 2, 'TCTG': 2, 'CAAA': 2, 'ACTG': 2, 'CTGC': 2, 'TGCC': 2, 'CATA': 1, 'ATAA': 1, 'TAAC': 1, 'ACGC': 1, 'CGCG': 1, 'GCGG': 1, 'CGGA': 1, 'GGAT': 1, 'ATGT': 1, 'TGTG': 1, 'GTGA': 1, 'TTTT': 1, 'TTTC': 1, 'TTCG': 1, 'TCGC': 1, 'CGCC': 1, 'GCCG': 1, 'CCGT': 1, 'CGTC': 1, 'GTCG': 1, 'TCGG': 1, 'CGGG': 1, 'GGGG': 1, 'GGGT': 1, 'GGTA': 1, 'TACG': 1, 'CGAT': 1, 'TTTG': 1, 'TTGA': 1, 'ATGA': 1, 'TGAC': 1, 'GACC': 1, 'ACCG': 1, 'CCGA': 1, 'CGAC': 1, 'GACG': 1, 'AACA': 1, 'ACAA': 1, 'CAAT': 1, 'AATC': 1, 'ATCT': 1, 'CTGG': 1, 'TGGC': 1, 'GGCA': 1, 'GCAA': 1, 'AAAG': 1, 'AAGT': 1, 'AGTA': 1, 'TACT': 1, 'GCCC': 1, 'CCCA': 1, 'CCAA': 1, 'AAAT': 1, 'AATG': 1, 'GCCA': 1, 'CCAC': 1,
              'CACT': 1, 'CTGT': 1, 'TGTT': 1, 'GTTC': 1, 'TTCT': 1})
 
+# kmer => groupes suivant les kmers les plus présents
+
 cp = cProfile.Profile()
 
 cp.enable()
 for _ in range(100000):
     sum([(a[elt]-b[elt])**2 for elt in set().union(*a, *b)])
-cp.disable()
 cp.print_stats()
+cp.disable()
 
 cp = cProfile.Profile()
 
@@ -21,8 +23,8 @@ cp.enable()
 for _ in range(100000):
     keys = [*a.keys(), *b.keys()]
     sum([abs(a[elt]-b[elt]) for elt in keys])
-cp.disable()
 cp.print_stats()
+cp.disable()
 
 cp = cProfile.Profile()
 
@@ -30,8 +32,8 @@ cp.enable()
 for _ in range(100000):
     a.subtract(b)
     sum([abs(v) for v in a.values()])
-cp.disable()
 cp.print_stats()
+cp.disable()
 
 
 cp = cProfile.Profile()
@@ -40,5 +42,5 @@ cp.enable()
 for _ in range(100000):
     a.subtract(b)
     sum()
-cp.disable()
 cp.print_stats()
+cp.disable()
